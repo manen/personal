fe2272dead3a143d6b32b6610e90f3e3447e4a39
manen <varga.bercel.i@gmail.com>
2021-03-18 21:28:57 +0100

    Customizability for projects

--- pages/donate.tsx
+++ pages/donate.tsx
@@ -1,37 +1,45 @@
 import React from "react";
 import Head from "next/head";
-import $ from "jquery";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
 
-const Donate: React.FC = () => (
-  <div>
-    <Head>
-      <title>Support my work - manen</title>
-    </Head>
-    <Header />
-    <div className="m-4">
-      <h1 className="font-black text-5xl">Donate</h1>
-      <div className="mt-1 text-sm text-accent-500 dark:text-accent-400">
-        <p>You can support my work by donating on PayPal.</p>
-      </div>
-      <div className="m-8">
-        <div className="m-4">
+const Donate: React.FC = () => {
+  const router = useRouter();
+  const { s } = router.query;
+
+  return (
+    <div>
+      <Head>
+        <title>Support {s || "my work"} - manen</title>
+      </Head>
+      <Header />
+      <div className="m-4">
+        <h1 className="font-black text-5xl">Donate{s ? " to " + s : ""}</h1>
+        <div className="mt-1 text-sm text-accent-500 dark:text-accent-400">
           <p>
-            While it's completely optional, it'd be <b>really</b> greatly
-            appreciated.
+            You can support {s || "my work"} by donating
+            {s ? " to manen" : ""} on PayPal.
           </p>
         </div>
-      </div>
-      <div className="h-full flex justify-center">
-        <a
-          target="_blank"
-          href="https://www.paypal.com/pools/c/8xPkV1n2Q2"
-          className="p-14 w-64 bg-accent-200 hover:bg-accent-300 dark:bg-accent-800 dark:hover:bg-accent-700 font-black text-xl transition-colors rounded-xl">
-          Donate now
-        </a>
+        <div className="m-8">
+          <div className="m-4">
+            <p>
+              While it's completely optional, it'd be <b>really</b> greatly
+              appreciated.
+            </p>
+          </div>
+        </div>
+        <div className="h-full flex justify-center">
+          <a
+            target="_blank"
+            href="https://www.paypal.com/pools/c/8xPkV1n2Q2"
+            className="px-14 py-8 bg-accent-200 hover:bg-accent-300 dark:bg-accent-800 dark:hover:bg-accent-700 text-xl transition-colors rounded-xl">
+            Donate now
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Donate;
